5be36308d4901f3d2994881cb8a7e04a98e02b39
Psych0o <140012249+psych0oR@users.noreply.github.com>
2025-03-20 10:28:05 +0200

    Add files via upload

---
+++ tahadi launcher/Form1.cs
@@ -0,0 +1,54 @@
+﻿/*
+#################################################################
+##                                                             ##
+##                                                             ##
+##  خاص بمشروع تطوير أسدا2 - يسمح بإعادة التعديل والنـشر   ##
+##                                                             ##
+##   لانشر تحدي - يسمح للاعب بتشغيل برنامج تسجيل الحسابات    ##
+##                                                             ##
+#################################################################
+*/
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace tahadi_launcher
+{
+    public partial class Form1 : Form
+    {
+        public static string ServerIp = "192.168.1.11"; //اتركه كما هو فقط حتى لا تحدث مشكلة أثناء تشغيل البرنامج
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(@"system\Asda2.exe", "CDLDKJFRE342LDLSL39"); //تشغيل الإصدار العربي
+                if (Process.GetProcesses().All((Process p) => !p.ProcessName.ToLower().Contains("vcharmgr.exe")))
+                {
+                Process.Start("vchrmgr.exe", Form1.ServerIp); //تشغيل برنامج تسجيل الحسابات
+                }
+                Process.GetCurrentProcess().Kill();
+            }
+            catch (Exception) {
+                //إن لم يتم العثور على إحدى البرنامجين بالأعلى سوف يظهر هذا الخطأ
+                MessageBox.Show("عذرا,لم يتم إيجاد البرنامج");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //الخروج من البرنامج
+        }
+    }
+}
---
+++ tahadi launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("tahadi launcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tahadi launcher")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65db0b59-c0d4-4e54-8a3e-93e637c0872c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
